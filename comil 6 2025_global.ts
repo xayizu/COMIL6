c667085365898e627ea5704643fbd87e26e73350
xayizu <xayizu@gmail.com>
2025-06-16 18:03:09 -0500

    cuarto commit del tour virtual con soporte Git LFS

--- comil 6 2025_global.ts
+++ comil 6 2025_global.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35422c2c08485358c9b43dd970169485c6d7a19382dfb59052e03854f9e2abf6
-size 238147429
+oid sha256:e82d6603d6d30fe176de25675fe48effbce0ef713a3bbc5bd6d1fdc5cf8c2dcb
+size 238682681
